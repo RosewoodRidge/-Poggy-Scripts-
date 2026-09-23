c1b4250c9f658f180be2eaad96efdd5502fc40ab
RosewoodRidge <admin@rosewoodridge.xyz>
2026-09-23 17:13:43 +0800

    Publish poggy_fishing v1.3.3, poggy_fishing_journal v1.2.3

--- [poggy_fishing_pack]/poggy_fishing_journal/fxmanifest.lua
+++ [poggy_fishing_pack]/poggy_fishing_journal/fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 'yes'
 poggy_id 'poggy_fishing_journal'
 author 'Poggy'
 description 'Fishing journal with a discoverable fish map and seasonal fish.'
-version '1.2.2'
+version '1.2.3'
 poggy_core_min '0.13.0'
 
 dependencies {
